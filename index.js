6ca8deb5fd89ccd0c3c7ec630dc951fd32771907
muhammadawaiis <157340031+muhammadawaiis@users.noreply.github.com>
2024-03-24 10:50:47 +0500

    Pushed my calculator on Github

---
+++ index.js
@@ -0,0 +1,28 @@
+#! /usr/bin/env  node    
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { massage: "Enter First Number", type: "number", name: "FirstNumber" },
+    { massage: "Enter Second Number", type: "number", name: "SecondNumber" },
+    {
+        massage: "Enter any one operator from list",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Divison"],
+    },
+]);
+// Condation perfome
+if (answer.operator === "Addition") {
+    console.log(answer.FirstNumber + answer.SecondNumber);
+}
+else if (answer.operator === "Subtraction") {
+    console.log(answer.FirstNumber - answer.SecondNumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.FirstNumber * answer.SecondNumber);
+}
+else if (answer.operator === "Divison") {
+    console.log(answer.FirstNumber / answer.SecondNumber);
+}
+else {
+    console.log("Select any Opreator");
+}
